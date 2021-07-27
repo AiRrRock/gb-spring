2ff87bc7d3076d8be97210e0f75897ec1e972a2f
Airt <alex.borichev@gmail.com>
2021-07-27 13:30:36 +0300

    Completed hw1

---
+++ src/main/java/com/geekbrains/app/Product.java
@@ -0,0 +1,25 @@
+package com.geekbrains.app;
+
+import java.io.Serializable;
+
+
+public class Product implements Serializable {
+    private int id;
+    private String title;
+    private double cost;
+
+    public Product(int id, String title, double cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
