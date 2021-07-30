a0e85e4cc9caaa49d13a6b7747dea239a2ece511
Airt <alex.borichev@gmail.com>
2021-07-30 18:12:37 +0300

    Completed hw2

---
+++ spring-core/src/main/java/ru/gb/lesson2/Product.java
@@ -0,0 +1,46 @@
+package ru.gb.lesson2;
+
+public class Product {
+    long id;
+    String name;
+    double price;
+
+    public Product(long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ru.gb.lesson2.Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
