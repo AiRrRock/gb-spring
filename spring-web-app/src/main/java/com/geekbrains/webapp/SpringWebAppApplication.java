e6a288fc18f54fe731fae961e6cca0e53e1ff5c6
AirRrock <alex.borichev@gmail.com>
2021-09-04 16:37:45 +0300

    Pre hw 12

---
+++ spring-web-app/src/main/java/com/geekbrains/webapp/SpringWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.geekbrains.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+	// Домашнее задание:
+	// 1. Сделать регистрацию пользователей на отдельной странице
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+}
